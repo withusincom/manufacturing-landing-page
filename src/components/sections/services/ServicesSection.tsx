9c00cb8b2eacfdd5f9b8750d5b0cd76a9ff97cb0
changwon <cwheo@withusin.com>
2026-01-16 16:09:09 +0900

    - refactoring: service section motion 코드 필요한 것만 작성

--- src/components/sections/services/ServicesSection.tsx
+++ src/components/sections/services/ServicesSection.tsx
@@ -1,23 +1,18 @@
-import { motion } from 'motion/react'
-import { ServiceItem } from './ServiceItem'
 import { SERVICES } from '@/constants/services'
+import { ServiceItem } from './ServiceItem'
 
 const ServicesSection = () => {
   return (
-    <motion.section
+    <section
       id="solutions"
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true }}
-      transition={{ staggerChildren: 0.2 }}
       className="flex justify-center items-start gap-8 px-4 lg:px-8 max-w-[1280px] mx-auto"
     >
       <div className="flex-1 max-w-[922px]">
         {SERVICES.map((service, index) => (
-          <ServiceItem key={service.id} service={service} index={index} />
+          <ServiceItem key={service.id} service={service} />
         ))}
       </div>
-    </motion.section>
+    </section>
   )
 }
 
